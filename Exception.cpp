0b17b82adf9ff4f1ec9823faa0635e6181c6a39e
GuyMiz95 <109472181+GuyMiz95@users.noreply.github.com>
2024-04-17 11:53:55 +0300

    Update Exception.cpp

--- Exception.cpp
+++ Exception.cpp
@@ -1,48 +1,48 @@
 #include "Exception.h"
 
 Exception::Exception(ERR_KIND kind) {
-  	_kind = kind;
+    _kind = kind;
 	switch(kind){
-	case INVALID_FUNC_ERR:
-	  _message = INVALID_FUNC_ERR_MSG;
-	  _type = LIB_ERR;
-	  break;
-	case MAX_THREAD_ERR:
-	  _message = MAX_THREAD_ERR_MSG;
-	  _type = LIB_ERR;
-	  break;
-    case INVALID_TID_ERR:
-      _message = INVALID_TID_ERR_MSG;
-      _type = LIB_ERR;
-      break;
-    case MAIN_THREAD_BLOCK_ERR:
-        _message = MAIN_THREAD_BLOCK_ERR_MSG;
-        _type = LIB_ERR;
-        break;
-    case QUANTUM_ERR:
-        _message = QUANTUM_ERR_MSG;
-        _type = LIB_ERR;
-        break;
-    case MAIN_THREAD_SLEEP_ERR:
-        _message = MAIN_THREAD_SLEEP_ERR_MSG;
-        _type = LIB_ERR;
-        break;
-	case TIMER_ERR:
-        _message = TIMER_ERR_MSG;
-        _type = SYS_ERR;
-        break;
-	case SIGSET_ERR:
-	    _message = SIGSET_ERR_MSG;
-	    _type = SYS_ERR;
-	    break;
-	case SIGACT_ERR:
-	    _message = SIGACT_ERR_MSG;
-	    _type = SYS_ERR;
-	    break;
-	case MEM_ERR:
-	    _message = MEM_ERR_MSG;
-	    _type = SYS_ERR;
-	}
+		case INVALID_FUNC_ERR:
+	        _message = INVALID_FUNC_ERR_MSG;
+	        _type = LIB_ERR;
+	        break;
+	    case MAX_THREAD_ERR:
+	        _message = MAX_THREAD_ERR_MSG;
+	        _type = LIB_ERR;
+	        break;
+        case INVALID_TID_ERR:
+             _message = INVALID_TID_ERR_MSG;
+             _type = LIB_ERR;
+             break;
+        case MAIN_THREAD_BLOCK_ERR:
+            _message = MAIN_THREAD_BLOCK_ERR_MSG;
+            _type = LIB_ERR;
+            break;
+        case QUANTUM_ERR:
+            _message = QUANTUM_ERR_MSG;
+            _type = LIB_ERR;
+            break;
+        case MAIN_THREAD_SLEEP_ERR:
+            _message = MAIN_THREAD_SLEEP_ERR_MSG;
+            _type = LIB_ERR;
+            break;
+    	case TIMER_ERR:
+            _message = TIMER_ERR_MSG;
+            _type = SYS_ERR;
+            break;
+    	case SIGSET_ERR:
+    	    _message = SIGSET_ERR_MSG;
+    	    _type = SYS_ERR;
+    	    break;
+    	case SIGACT_ERR:
+    	    _message = SIGACT_ERR_MSG;
+    	    _type = SYS_ERR;
+    	    break;
+    	case MEM_ERR:
+    	    _message = MEM_ERR_MSG;
+    	    _type = SYS_ERR;
+    }
 }
 
 void Exception::print_error() {
